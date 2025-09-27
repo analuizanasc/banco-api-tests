e281bc645d9c8311986e700bb6b470b2a3c0eea7
Ana Luiza <analuiza_04@hotmail.com>
2025-09-27 17:38:23 -0300

    add .env

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require( 'dotenv').config()
 
 describe('Transferências', () => {
     describe('POST / transferencias', () => {
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -15,7 +16,7 @@ describe('Transferências', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Barear ${token}`)
@@ -29,7 +30,7 @@ describe('Transferências', () => {
             expect(resposta.statusCode).to.be.equal(201)
         })
         it('Deve retornar sucesso com 402 quando o valor da transferência for acima de R$ 10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -39,7 +40,7 @@ describe('Transferências', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Barear ${token}`)
