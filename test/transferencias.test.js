766ed0334de8d7631ac5fe9a03b20ff74c425e3f
Ana Luiza <analuiza_04@hotmail.com>
2025-09-27 18:48:10 -0300

    add fixtures

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require( 'dotenv').config();
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencia.json')
 
 describe('Transferências', () => {
     describe('POST / transferencias', () => {
@@ -12,32 +13,25 @@ describe('Transferências', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
+            const bodyTransferencia = {...postTransferencias}
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Barear ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 10,
-                    token: token
-                })
+                .send(bodyTransferencia)
 
             expect(resposta.statusCode).to.be.equal(201)
         })
         it('Deve retornar sucesso com 402 quando o valor da transferência for acima de R$ 10,00', async () => {
-            const token = await obterToken( 'julio.lima', '123456')
+            const bodyTransferencia = {...postTransferencias}
+            bodyTransferencia.valor = 7
 
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Barear ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 7,
-                    token: token
-                })
+                .send(bodyTransferencia)
 
             expect(resposta.statusCode).to.be.equal(422)
         })
